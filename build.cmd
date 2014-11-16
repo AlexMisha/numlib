381fcd164e9c1d22330a4278cdc36e842189b4c4
Misha <michaelalex600@gmail.com>
2014-11-16 23:05:44 +0300

    Билд-файл для сборки библиотек

--- build.cmd
+++ build.cmd
@@ -1,28 +1,54 @@
 @echo off
 
-ml /c /coff Lenstr.asm || goto ErrLable
+ml /c /coff c:\masm32\projects\numlib\src\main\Lenstr.asm || goto ErrLable
 
 lib /subsystem:windows /export:Lenstr Lenstr.obj
 
-ml /c /coff ReverStr.asm || goto ErrLable
+ml /c /coff c:\masm32\projects\numlib\src\main\ReverStr.asm || goto ErrLable
 
 lib /subsystem:windows /export:ReverStr ReverStr.obj
 
-ml /c /coff StrToNum.asm || goto ErrLable
+ml /c /coff c:\masm32\projects\numlib\src\main\StrToNum.asm || goto ErrLable
 
 lib /subsystem:windows /export:StrToNum StrToNum.obj
 
-ml /c /coff Reconversion.asm || goto ErrLable
+ml /c /coff c:\masm32\projects\numlib\src\main\Reconversion.asm || goto ErrLable
 
 lib /subsystem:windows /export:Reconversion Reconversion.obj
 
-ml /c /coff TestStr.asm || goto ErrLable
+ml /c /coff c:\masm32\projects\numlib\src\main\TestStr.asm || goto ErrLable
 
 lib /subsystem:windows /export:TestStr TestStr.obj
 
-ml /c /coff SizeOfNum.asm || goto ErrLable
+ml /c /coff c:\masm32\projects\numlib\src\main\SizeOfNum.asm || goto ErrLable
 
 lib /subsystem:windows /export:SizeOfNum SizeOfNum.obj
+
+del Lenstr.obj || goto ErrLable
+
+del ReverStr.obj || goto ErrLable
+
+del Calculator.obj || goto ErrLable
+
+del Lenstr.lib || goto ErrLable
+
+del ReverStr.lib || goto ErrLable
+
+del StrToNum.obj || goto ErrLable
+
+del StrToNUm.lib || goto ErrLable
+
+del SizeOfNum.obj || goto ErrLable
+
+del SizeOfNum.lib || goto ErrLable
+
+del Reconversion.obj || goto ErrLable
+
+del Reconversion.lib || goto ErrLable
+
+del TestStr.obj || goto ErrLable
+
+del TestStr.lib || goto ErrLable
 
 goto TheEnd
 
