1fd0cb745d15a4aca8083fa0c6ee85910654ad35
Misha <michaelalex600@gmail.com>
2014-12-02 20:57:46 +0300

    сделал так, чтобы функа обрабатывала бесконечно большие числа

--- src/main/StrToNum.asm
+++ src/main/StrToNum.asm
@@ -12,9 +12,10 @@ PUBLIC StrToNum
 .data
 
 .code
+; ebp+12 - буффер под результат
 ; ebp+8 - строка
 ; результат помещён в буффер со строкой в инвертированном виде
-StrToNum proc pStr:dword
+StrToNum proc pStr:dword, pRes:dword
 mov esi, pStr									; указатель на буффер со строкой
 ; размер строки
 push pStr
@@ -24,14 +25,17 @@ add esi, ebx										; указатель на последний символ буффера
 sub esi,3h											; указатель на последнюю цифру буффера
 mov ecx, 1h
 sub ebx, 2h
-mov edi, 0
+mov edi, pRes
 ; цикл преобразования
 StartConvertionCicle:
-mov eax, 0
+mov eax,0
 mov al, byte ptr [esi]							
 sub eax, 30h
 mul ecx
-add edi, eax
+mov edx, dword ptr [edi]
+add eax,edx
+mov dword ptr [edi], eax
+xor edx,edx
 mov eax, 10
 mul ecx
 mov ecx, eax
@@ -43,8 +47,7 @@ jmp StartConvertionCicle
 EndConvertionCicle:
 
 Return:
-mov [esi], edi
-mov edi, 0											; сбалансировать стэк
+										; сбалансировать стэк
 ret													; вытащить из стэка 1 аргумент
 StrToNum endp
 end
