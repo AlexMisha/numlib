1b93b87ce519a3e99eeb5d2c34c3fe5239b1912a
Misha <michaelalex600@gmail.com>
2015-03-08 10:21:09 +0300

    ОСновательно изменена библиотека

--- src/main/StrToNum.asm
+++ src/main/StrToNum.asm
@@ -6,52 +6,34 @@ PUBLIC StrToNum
    includelib c:\masm32\lib\user32.lib
    includelib c:\masm32\lib\kernel32.lib
    includelib c:\masm32\projects\calculator\Lenstr.lib
-
+   includelib c:\masm32\projects\numlib\atodw.lib
+   
+   extern atodw@4:near
    extern Lenstr@4:near
 
 .data
 
 .code
-; ebp+12 - буффер под результат
 ; ebp+8 - строка
-; результат помещён в буффер со строкой в инвертированном виде
-StrToNum proc pStr:dword, pRes:dword
+StrToNum proc pStr:dword
 mov esi, pStr									; указатель на буффер со строкой
 ; размер строки
 push pStr
 call Lenstr@4
-
-
-
-MainCode:
-; входные данные для цикла
-add esi, ebx										; указатель на последний символ буффера						
-sub esi,3h											; указатель на последнюю цифру буффера
-mov ecx, 1h
 sub ebx, 2h
-mov edi, pRes
-; цикл преобразования
-StartConvertionCicle:
-mov eax,0
-mov al, byte ptr [esi]							
-sub eax, 30h
-mul ecx
-mov edx, dword ptr [edi]
-add eax,edx
-mov dword ptr [edi], eax
-xor edx,edx
-mov eax, 10
-mul ecx
-mov ecx, eax
-dec ebx
-sub esi, 1h
-cmp ebx, 0
-je EndConvertionCicle
-jmp StartConvertionCicle
-EndConvertionCicle:
+
+Masm32Code:
+mov esi,pStr
+add esi,ebx
+mov byte ptr [esi], 0
+inc esi
+mov byte ptr [esi], 0
+push pStr
+call atodw@4
+jmp Return
 
 Return:
 										; сбалансировать стэк
 ret													; вытащить из стэка 1 аргумент
 StrToNum endp
-end
+end 
